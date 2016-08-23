faa376ce3e62db32ab33080e062363d8a23a5dce
Balbina <balbinasantana@gmail.com>
2016-08-23 18:40:29 -0500

    Only one user can register per event per day

--- app/controllers/event/sessions_controller.rb
+++ app/controllers/event/sessions_controller.rb
@@ -2,17 +2,15 @@ class Event::SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     @user = User.create(user_params) if @user.nil?
-    # if Attendee.find_by('created_at >= ? AND created_at <= ?', Time.zone.today.beginning_of_day, Time.zone.today.end_of_day)
-    #   respond_to do |format|
-    #     format.html{ redirect_to event_register_path, alert: 'Correo ya registrado para este evento, trata con otro.' }
-    #   end
-    # elsif Attendee.create(user: @user, event: current_event)
-    if Attendee.create(user: @user, event: current_event)
+    @attendee = Attendee.new(user: @user, event: current_event)
+    if @attendee.save
       respond_to do |format|
         format.js
       end
     else
-      render event_register_path
+      respond_to do |format|
+        format.html{ redirect_to event_register_path, alert: 'Correo ya registrado para este evento, trata con otro.' }
+      end
     end
   end
 
--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,4 +1,14 @@
 class Attendee < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  validate :email_is_unique_today
+
+  private
+  def email_is_unique_today
+    if Attendee.where(user: user).where('created_at > (?)', Time.zone.today.beginning_of_day).count > 0
+      errors.add(:email, 'Correo ya registrado')
+    end
+  end
+
 end
