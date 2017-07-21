5518789a6a1ae7fe6ee1914fddb46fbaf81d5603
gdavila09 <grrd99@gmail.com>
2017-07-21 11:21:52 -0500

    fixed rubocop

--- app/controllers/event/boletia_api_controller.rb
+++ app/controllers/event/boletia_api_controller.rb
@@ -3,7 +3,7 @@ class Event::BoletiaApiController < ApplicationController
     @response = RestClient::Request.execute(
       method: :get,
       url: "https://boletiaapi.com/api/v1/search/events/#{params[:boletia_id]}/tickets?number=#{params[:id]}",
-      headers: { 'authorization' => 'Bearer a52a81ff-837d-4f25-8d82-1aefee825103' },
+      headers: { authorization: 'Bearer a52a81ff-837d-4f25-8d82-1aefee825103' },
       verify_ssl: OpenSSL::SSL::VERIFY_NONE
     )
     render json: @response
